e158594cb9947610eff4022438c2c9d12db7e9b4
Jakub Andrzejewski <jakubandrz1@gmail.com>
2020-12-01 21:00:57 +0100

    test

--- task/task1/example.js
+++ task/task1/example.js
@@ -1,4 +1,4 @@
-export const alphabet = 'abcdefghijklmnoprstuwxyz'.split('')
+export const alphabet = 'abcdefghijklmnoprstuwxyzzaqwewqeqw'.split('')
 
 //function to get random size of chunks
 export function randomChunkSize(minSize, maxSize) {
