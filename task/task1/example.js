a885ae8a03696149a6e96c9c9287ee67ba80bbb9
Jakub Andrzejewski <jakubandrz1@gmail.com>
2020-12-01 20:42:22 +0100

    Changes

--- task/task1/example.js
+++ task/task1/example.js
@@ -1,4 +1,4 @@
-export const alphabet = 'abcdefghijklmnoprstuwxyz'.split('')
+export const alphabet = 'abcdefghijklmnoprstuwxyzyzsssassss'.split('')
 
 export function randomNumber(min, max) {
     return Math.round(Math.random() * (max - min) + min);
