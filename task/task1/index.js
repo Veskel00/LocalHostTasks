fb9807cdd1b99c967cdf62afdc264c941b3a1fd6
Jakub Andrzejewski <jakubandrz1@gmail.com>
2020-12-04 21:24:37 +0100

    End of Task 1

--- task/task1/index.js
+++ task/task1/index.js
@@ -2,7 +2,6 @@ import { randomChunkSize } from './example';
 
 function aggregateIntoChunks(array) {
   const arrayOfChunks = [];
-  console.log('test');
   while (array.length) {
     const singleChunk = array.splice(0, randomChunkSize(4, 7));
     if (singleChunk.length < 4) {
