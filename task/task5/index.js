9a2af9c5cdce9a4ae79c0a541d2c5f0aba602266
Jakub Andrzejewski <jakubandrz1@gmail.com>
2020-12-06 19:55:05 +0100

    Test

--- task/task5/index.js
+++ task/task5/index.js
@@ -1,18 +1,18 @@
 //function to get random size of numbers in array;
 
-import randomSize from './example';
+// import randomSize from './example';
 
-//howManyNumber = ile indexow w tablicy, min,max - losowa liczba w poszczególnym indexie
-export function generateArrayWithRandomNumbers(howManyNumbers, min, max) {
-  const arrayWithRandomNumbers = Array.from({ length: howManyNumbers }, () => randomSize(min, max));
-  return arrayWithRandomNumbers;
-}
+// //howManyNumber = ile indexow w tablicy, min,max - losowa liczba w poszczególnym indexie
+// export function generateArrayWithRandomNumbers(howManyNumbers, min, max) {
+//   const arrayWithRandomNumbers = Array.from({ length: howManyNumbers }, () => randomSize(min, max));
+//   return arrayWithRandomNumbers;
+// }
 
-export function generateArrayOfArrays(howManyArrays, howManyNumbers, min, max) {
-  const arrayOfResults = [];
+// export function generateArrayOfArrays(howManyArrays, howManyNumbers, min, max) {
+//   const arrayOfResults = [];
 
-  for (let i = 0; i < howManyArrays; i++) {
-    arrayOfResults.push(Array.from({ length: howManyNumbers }, () => randomSize(min, max)));
-  }
-  return arrayOfResults;
-}
+//   for (let i = 0; i < howManyArrays; i++) {
+//     arrayOfResults.push(Array.from({ length: howManyNumbers }, () => randomSize(min, max)));
+//   }
+//   return arrayOfResults;
+// }
